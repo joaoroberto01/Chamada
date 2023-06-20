b2e40a081386de4a03ba8b70e37d96c68817cb06
joaoroberto <joaoroberto.gc.01@gmail.com>
2023-06-19 21:53:58 -0300

    - Adding fucking finesse 4 (IP LUIS)

--- lib/shared/environment.dart
+++ lib/shared/environment.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const String BASE_URL = 'http://172.25.238.209:8080/chamada';
+  static const String BASE_URL = 'http://172.25.92.37:8080/chamada';
   //192.168.195.203
   //172.25.238.209
 }
